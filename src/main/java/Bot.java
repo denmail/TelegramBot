4cde5ded7167682d9fc3158a35f56176d5ef7be9
denkoi <kotkin8@gmail.com>
2022-09-09 00:53:39 +0300

    пушу хуйню

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Bot {
+import lombok.Getter;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public final class Bot extends TelegramLongPollingCommandBot {
+
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+
+    public Bot(String botName, String botToken) {
+        super();
+        this.BOT_NAME = botName;
+        this.BOT_TOKEN = botToken;
+
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    /**
+     * Ответ на запрос, не являющийся командой
+     */
+    @Override
+    public void processNonCommandUpdate(Update update) {
+        Message msg = update.getMessage();
+        Long chatId = msg.getChatId();
+    }
+
 }
--- src/main/java/TelebotApplication.java
+++ src/main/java/TelebotApplication.java
@@ -1,6 +1,19 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Map;
+
 public class TelebotApplication {
+    private static final Map<String, String> getenv = System.getenv();
 
     public static void main(String[] args) {
-        System.out.println("first output");
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot("PrimathBot", "5659995448:AAHaKYXPtn9lGPbEuHzaG_CskyeAQUl80Sc"));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
     }
 }
