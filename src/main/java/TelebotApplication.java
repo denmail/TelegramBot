c8f54bba3dc1f9a7e518cc0a178e1f945178a146
asaturovdg <asaturovdg@gmail.com>
2022-09-09 03:30:11 +0300

    ogo

--- src/main/java/TelebotApplication.java
+++ src/main/java/TelebotApplication.java
@@ -1,19 +1,2 @@
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import java.util.Map;
-
 public class TelebotApplication {
-    private static final Map<String, String> getenv = System.getenv();
-
-    public static void main(String[] args) {
-        try {
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new Bot("PrimathBot", "5659995448:AAHaKYXPtn9lGPbEuHzaG_CskyeAQUl80Sc"));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
