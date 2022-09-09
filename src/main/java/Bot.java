a04fc879023e576683b5145a3dec160a19410880
denkoi <kotkin8@gmail.com>
2022-09-09 17:36:45 +0300

    нихуя не сделал2

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,12 +1,10 @@
-import lombok.Getter;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public final class Bot extends TelegramLongPollingBot {
-
     private final String BOT_NAME;
     private final String BOT_TOKEN;
 
@@ -30,22 +28,17 @@ public final class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         try {
-            //проверяем есть ли сообщение и текстовое ли оно
+            NotificationKeyboard notificationKeyboard = new NotificationKeyboard();
             if (update.hasMessage() && update.getMessage().hasText()) {
-                //Извлекаем объект входящего сообщения
-                Message inMessage = update.getMessage();
-                //Создаем исходящее сообщение
-                SendMessage outMessage = new SendMessage();
-                //Указываем в какой чат будем отправлять сообщение 
-                //(в тот же чат, откуда пришло входящее сообщение)
-                outMessage.setChatId(inMessage.getChatId());
-                //Указываем текст сообщения
-                outMessage.setText(inMessage.getText());
-                //Отправляем сообщение
-                execute(outMessage);
+
             }
-        } catch (TelegramApiException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public void exeSendPhoto() {
+
+    }
+
 }
