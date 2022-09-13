3e0d05387b11f44c49c88971a3a8989b4884e3d7
asaturovdg <asaturovdg@gmail.com>
2022-09-14 01:30:11 +0300

    MySql > Postgresql

--- src/main/java/ru/primath/Manager/DBManager.java
+++ src/main/java/ru/primath/Manager/DBManager.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 
 public class DBManager {
 
-    static String url = "jdbc:mysql://localhost:5432/primath_db";
+    static String url = "jdbc:mysql://localhost/primath_db";
     static String user = "admin";
     static String password = "admin";
 
@@ -37,14 +37,15 @@ public class DBManager {
         try (Connection con = DriverManager.getConnection(url, user, password);
              PreparedStatement pst = con.prepareStatement("SELECT * FROM makara");
              ResultSet rs = pst.executeQuery()) {
-            while (rs.next()) {
+            /*while (rs.next()) {
                 Primat primat = new Primat(rs.getByte(2),
                         rs.getString(1),
                         rs.getString(3),
                         rs.getLong(4),
                         rs.getString(5));
                 PrimatManager.addPrimat(primat);
-            }
+            }*/
+            System.out.println("DATATATATA");
 
         } catch (SQLException ex) {
             Logger lgr = Logger.getLogger(TelebotApplication.class.getName());
