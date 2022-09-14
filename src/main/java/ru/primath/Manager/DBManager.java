e0b9d3ca8f1e2e8a0b742bde4821cc63356d65c1
asaturovdg <asaturovdg@gmail.com>
2022-09-14 10:25:24 +0300

    db commit #inf

--- src/main/java/ru/primath/Manager/DBManager.java
+++ src/main/java/ru/primath/Manager/DBManager.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 public class DBManager {
 
     static String url = "jdbc:mysql://localhost/primath_db";
-    static String user = "admin";
+    static String user = "mysqlG";
     static String password = "admin";
 
 
@@ -45,7 +45,7 @@ public class DBManager {
                         rs.getString(5));
                 PrimatManager.addPrimat(primat);
             }
-            System.out.println("LoadFromDB");
+            System.out.println("LOADED FROM DB");
 
         } catch (SQLException ex) {
             Logger lgr = Logger.getLogger(TelebotApplication.class.getName());
